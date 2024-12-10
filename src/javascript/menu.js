dbf7b983a6689a3c75f1b5bbad4b932e4546bbbe
Minerva <superetro85@gmail.com>
2024-12-10 00:42:04 -0300

    Add: Menu Script (0.2)

--- src/javascript/menu.js
+++ src/javascript/menu.js
@@ -1,15 +1,35 @@
 // Catch
 const menu = document.getElementById("menu");
+const items = document.getElementById("menu_items");
 
 menu.addEventListener("click", function(e){
-    const items = document.getElementById("menu_items");
 
-    const currentDisplay = window.getComputedStyle(items).display;
-    const currentHeight = window.getComputedStyle(items).height
 
-    items.classList.toggle("show")
-   
-    
-    
-
+    if(items.style.height == '0vh' || items.style.height === ''){
+        requestAnimationFrame(() =>{
+            animarMenu()
+        })
+    } else {
+        requestAnimationFrame(() => {
+            menu.style.transition = "transform 2s ease"
+            items.style.height = '0vh'
+        })
+    }
 })
+
+function animarMenu(){
+    const menu = document.getElementById("menu_items")
+
+    menu.style.transition = "height 0.3s ease";
+    menu.style.height = '50vh'
+}
+
+window.addEventListener("resize", function() {
+    const items = document.getElementById("menu_items");
+    const menu = document.getElementById("menu");
+    if (window.innerWidth >= 774) { 
+        items.style.height = 'auto';
+    } else {
+        items.style.height = '0vh';
+    }
+});
