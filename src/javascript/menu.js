e4ff861f870581de9b7e5a3b5ccd3efa8391bdd1
Minerva <superetro85@gmail.com>
2024-12-11 23:24:11 -0300

    TOols

--- src/javascript/menu.js
+++ src/javascript/menu.js
@@ -15,21 +15,6 @@ menu.addEventListener("click", function(e){
             items.style.height = '0vh'
         })
     }
-})
-
-function animarMenu(){
-    const menu = document.getElementById("menu_items")
-
-    menu.style.transition = "height 0.3s ease";
-    menu.style.height = '50vh'
-}
-
-window.addEventListener("resize", function() {
-    const items = document.getElementById("menu_items");
-    const menu = document.getElementById("menu");
-    if (window.innerWidth >= 774) { 
-        items.style.height = 'auto';
-    } else {
-        items.style.height = '0vh';
+})bran        items.style.height = '0vh';
     }
 });
