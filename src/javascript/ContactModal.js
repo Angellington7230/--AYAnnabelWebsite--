a9de50f2133220cba03cd7f62e7c2bd567a94a69
Seu Nome de Usuário <superetro85@gmail.com>
2025-01-14 16:31:14 -0500

    Add: Page Contacts

---
+++ src/javascript/ContactModal.js
@@ -0,0 +1,7 @@
+const modal = document.getElementById('contact-modal');
+
+const btn_modal = document.getElementById('btn-modal');
+
+btn_modal.addEventListener('click', () => {
+  modal.showModal();
+});
