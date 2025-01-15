e467e90c61a20b2cf5537283c04f93d91f42000f
Seu Nome de Usuário <superetro85@gmail.com>
2025-01-15 15:56:32 -0500

    Conact form styles

--- src/javascript/ContactModal.js
+++ src/javascript/ContactModal.js
@@ -1,42 +1,51 @@
-const modal = document.getElementById('contact-modal');
+// Catch the form and the modal
+const form = document.getElementById("form");
+const dialog = document.getElementById("contact-modal");
 
-const btn_modal = document.getElementById('btn-modal');
+// Buttons on the modal
+const send_letter = document.getElementsByClassName("send_letter")[0];  
+const back_letter = document.getElementsByClassName("back_letter")[0];
 
-btn_modal.addEventListener('click', () => {
-  modal.showModal();
-});
+// Add an event listener to the form 
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  
+  // catching the form data
+  let name = document.getElementById("name").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let message = document.getElementById("message").value.trim();
+
+  // Place in Modal
+
+  let name_modal = document.getElementById("modal_name");
+  let email_modal = document.getElementById("modal_email");
+  let message_modal = document.getElementById("modal_message");
+
+  name_modal.innerHTML = name;
+  email_modal.innerHTML = email;
+  message_modal.innerHTML = message;
 
 
- // Carregar com o DOMContentLoader 
- /*
- Cada ponot do fomudmular, form, modal
+
+  dialog.showModal();
+
+    // Close Modal
+  back_letter.addEventListener("click", () => {
+    dialog.close();
+  })
+
+  // Send Modal
+  send_letter.addEventListener("click", () => {
+    dialog.close();
+  })
+})
 
 
- aí quando abrir o form, vai por os valores em form
-
- vai atualizar o conteudo do modal logo após enviar
 
 
- depois quando clicar fora do modal, vai fechar
- 
- */
 
 
- document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById("form");
-    const modal = document.getElementById("modal");
 
-    const modal_name = document.getElementById("modal_name");
-    const modal_email = document.getElementById("modal_email");
-    const modal_message = document.getElementById("modal_message");
-
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-
-        modal_name.textContent = form.name.value;
-        modal_email.textContent = form.email.value;
-        modal_message.textContent = form.message.value;
-
-        modal.showModal();
-    })
- });
+// window.addEventListener("DOMContentLoaded", () => {
+//   dialog.showModal(); 
+// });
