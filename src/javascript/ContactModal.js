ca46edcdf53111e4e1169a398cef176036cc88e7
Seu Nome de Usuário <superetro85@gmail.com>
2025-01-17 14:22:07 -0500

    Add: Conact Finish

--- src/javascript/ContactModal.js
+++ src/javascript/ContactModal.js
@@ -1,3 +1,30 @@
+// Public key email
+emailjs.init("bOfHdKpZptqmRrs5o")
+
+function sendEmail(){
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const message = document.getElementById("message").value.trim();
+
+  // Parameters for TemplateJS
+  const templateParams = {
+    from_name: name,
+    from_email: email,
+    message: message,
+  };
+  emailjs.send("service_ykl66y3", "template_04t71zf", templateParams)
+  .then((response) => {
+    console.log("Success: ", response);
+    alert("Message Sent!");
+    document.getElementById("form").reset();
+  }, (error) => {
+    console.log("Failed: ", error);
+    alert("Message Failed to Send!");
+  })
+}
+
+
+
 // Catch the form and the modal
 const form = document.getElementById("form");
 const dialog = document.getElementById("contact-modal");
@@ -5,6 +32,7 @@ const dialog = document.getElementById("contact-modal");
 // Buttons on the modal
 const send_letter = document.getElementsByClassName("send_letter")[0];  
 const back_letter = document.getElementsByClassName("back_letter")[0];
+
 
 // Add an event listener to the form 
 form.addEventListener("submit", (e) => {
@@ -36,18 +64,8 @@ form.addEventListener("submit", (e) => {
 
   // Send Modal
   send_letter.addEventListener("click", () => {
-    form.submit();
-    alert("Message Sent!");
+    sendEmail();
     dialog.close();
   })
 })
 
-
-
-
-
-
-
-// window.addEventListener("DOMContentLoaded", () => {
-//   dialog.showModal(); 
-// });
